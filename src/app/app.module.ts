9ed0a8c24659d86210ef046280ec434a8d20fae3
cleilsonjose <cleilsonrock@gmail.com>
2022-11-29 11:39:33 -0300

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 
 @NgModule({
@@ -47,7 +48,7 @@ import { DishService } from './services/dish.service';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
