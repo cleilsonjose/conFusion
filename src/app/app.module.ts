e089acf3c2b0775e4a54b4656bb65b151bf25884
cleilsonjose <cleilsonrock@gmail.com>
2022-12-01 10:46:39 -0300

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,10 @@ import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 import "hammerjs";
 import { DishService } from "./services/dish.service";
@@ -51,6 +55,10 @@ import { LeaderService } from "./services/leader.service";
     MatButtonModule,
     AppRoutingModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [
     DishService,
