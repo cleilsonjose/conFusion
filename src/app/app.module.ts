3a2193edde79e938447e9128a4887f3928efe3c6
cleilsonjose <cleilsonrock@gmail.com>
2022-12-01 10:02:37 -0300

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
 import { ContactComponent } from "./contact/contact.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { LoginComponent } from './login/login.component';
 
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -20,6 +21,7 @@ import { MatListModule } from "@angular/material/list";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from '@angular/material/dialog';
 
 import "hammerjs";
 import { DishService } from "./services/dish.service";
@@ -36,6 +38,7 @@ import { LeaderService } from "./services/leader.service";
     AboutComponent,
     HomeComponent,
     ContactComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -47,6 +50,7 @@ import { LeaderService } from "./services/leader.service";
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
+    MatDialogModule,
   ],
   providers: [
     DishService,
@@ -54,5 +58,8 @@ import { LeaderService } from "./services/leader.service";
     LeaderService
   ],
   bootstrap: [AppComponent],
+  entryComponents: [
+    LoginComponent
+],
 })
 export class AppModule {}
